560e0c53d41dee99afd92355b8d682e7a2043caf
Tobias Wiedau <t_wied05@wwu.de>
2014-02-26 15:17:15 +0100

    Player Singleton

--- Dezibel/src/de/dezibel/player/Player.java
+++ Dezibel/src/de/dezibel/player/Player.java
@@ -6,76 +6,78 @@ import de.dezibel.data.Playlist;
 
 public class Player {
 
-	private LinkedList<Medium> currentPlaylist;
+    private LinkedList<Medium> currentPlaylist;
 
-	private static Player instance = null;
+    private static Player instance = null;
 
-	private Player() {
+    private Player() {
 
-	}
+    }
 
-	public Player getInstance() {
-		return null;
-	}
+    public Player getInstance() {
+        if(instance == null)
+            instance = new Player();
+        return instance;
+    }
 
-	public void play() {
+    public void play() {
 
-	}
+    }
 
-	public void pause() {
+    public void pause() {
 
-	}
+    }
 
-	public void stop() {
+    public void stop() {
 
-	}
+    }
 
-	public void jumpTo(int pos) {
+    public void jumpTo(int pos) {
 
-	}
+    }
 
-	public void next() {
+    public void next() {
 
-	}
+    }
 
-	public void previous() {
+    public void previous() {
 
-	}
+    }
 
-	public int getVolume() {
-		return 0;
-	}
+    public int getVolume() {
+        return 0;
+    }
 
-	public void setVolume(int volume) {
+    public void setVolume(int volume) {
 
-	}
+    }
 
-	public boolean isPlaying() {
-		return false;
-	}
+    public boolean isPlaying() {
+        return false;
+    }
 
-	public Medium getCurrentMedium() {
-		return null;
-	}
+    public Medium getCurrentMedium() {
+        return null;
+    }
 
-	public void addMedium(Medium song) {
+    public void addMedium(Medium song) {
 
-	}
+    }
 
-	public void removeMedium(Medium song) {
+    public void removeMedium(Medium song) {
 
-	}
+    }
 
-	public void addPlaylist(Playlist playlist) {
+    public void addPlaylist(Playlist playlist) {
 
-	}
+    }
 
-	public void setPlaylist(Playlist playlist) {
+    public void setPlaylist(Playlist playlist) {
 
-	}
+    }
 
-	public void clearPlaylist() {
+    public void clearPlaylist() {
 
-	}
+    }
 
 }
