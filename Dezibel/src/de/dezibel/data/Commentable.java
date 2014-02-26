8949a6b5276281a9b5bfd1524611517bfec79a38
Henner25 <kein.regen@wolke7.net>
2014-02-26 17:00:55 +0100

    Comment&Commentable

--- Dezibel/src/de/dezibel/data/Commentable.java
+++ Dezibel/src/de/dezibel/data/Commentable.java
@@ -1,11 +1,26 @@
 package de.dezibel.data;
 
 import java.util.ArrayList;
-
+/**
+ * This interface must be implemented by all classes that can be commented.
+ * @author Henner
+ */
 public interface Commentable {
 
-	public abstract void comment(Comment comment);
+    /**
+     * Add the given <p>comment</p> to this object.
+     * @param comment 
+     * @pre self.comment != null
+     * @post true
+     */
+    public abstract void comment(Comment comment);
 
-	public abstract ArrayList<Comment> getComments();
+    /**
+     * Get all comments posted to this object.
+     * @return ArrayList containing all the posted comments.
+     * @pre true
+     * @post true
+     */
+    public abstract ArrayList<Comment> getComments();
 
 }
