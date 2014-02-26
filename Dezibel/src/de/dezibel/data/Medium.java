b60162eee3d855b103617c87a3c7a713ac9af121
Tobias Wiedau <t_wied05@wwu.de>
2014-02-26 15:24:59 +0100

    Player Singleton

--- Dezibel/src/de/dezibel/data/Medium.java
+++ Dezibel/src/de/dezibel/data/Medium.java
@@ -3,6 +3,7 @@ package de.dezibel.data;
 import de.dezibel.io.MediumLoader;
 import java.util.Date;
 import java.util.ArrayList;
+import java.io.File;
 
 public class Medium implements Commentable, Lockable {
 
--- Dezibel/src/de/dezibel/player/Player.java
+++ Dezibel/src/de/dezibel/player/Player.java
@@ -11,7 +11,7 @@ public class Player {
     private static Player instance = null;
 
     private Player() {
-
+        
     }
 
     public Player getInstance() {
