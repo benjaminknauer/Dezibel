c83303ca60fc3ad0ebed319d1ff5e930581b6cb2
schmokko <ben.knauer@gmail.com>
2014-02-27 10:03:20 +0100

    Playlist fertig

--- Dezibel/src/de/dezibel/data/Playlist.java
+++ Dezibel/src/de/dezibel/data/Playlist.java
@@ -2,15 +2,25 @@ package de.dezibel.data;
 
 import java.util.LinkedList;
 
-// TODO: Assoziationen
-
+/**
+ * This class represents a playlist, which can be created by a user.
+ * 
+ * @author Alexander Trahe, Benjamin Knauer
+ * @inv A playlist contains at least 1 medium.
+ */
 public class Playlist implements Commentable {
 
     private String titel;
     private LinkedList<Comment> comments;
     private LinkedList<Medium> mediumList;
     private User user;
-            
+    
+    /**
+     * Constructor of the playlistclass
+     * @param medium first Medium in the playlist
+     * @param titel title of the playlist
+     * @param user owner of the playlist
+     */
     public Playlist(Medium medium, String titel, User user) {
         this.titel = titel;
         this.mediumList = new LinkedList<>();
@@ -18,19 +28,37 @@ public class Playlist implements Commentable {
         this.user = user;
         this.user.addPlaylist(this);
     }
-
+    
+    /**
+     * This method adds a medium to the playlist.
+     * @param medium medium to add
+     */
     public void addMedium(Medium medium) {
         this.mediumList.add(medium);
     }
 
+    /**
+     * This method removes a medium from the playlist.
+     * @param index index of the medium in the list
+     * @pre list is not empty
+     * @post The size of the list has been reduced by 1.
+     */
     public void removeMedium(int index) {
+        if(index != -1){ // is a medium selected?
         mediumList.remove(index);
+        if(mediumList.isEmpty()){
+            Database.getInstance().removePlaylist(this);
+            user.removePlaylist(this);
+        }
+        }
     }
 
     /**
      * This method moves a mediaobject from it's current position to a new one.
      * @param currentPos The current position of the mediaobject which is to be moved.
      * @param newPos The position the mediaobject is supposed to be moved to.
+     * @pre currentPos and newPos are in range of 0 to mediumList.size()-1
+     * @post the medium is at newPos in mediumList
      */
     public void move(int currentPos, int newPos) {
         Medium temp = mediumList.get(currentPos);
@@ -42,6 +70,10 @@ public class Playlist implements Commentable {
         }
     }
 
+    /**
+     * This method returns the size of the playlist.
+     * @return size of playlist
+     */
     public int size() {
         return mediumList.size();
     }
@@ -58,8 +90,14 @@ public class Playlist implements Commentable {
         this.titel = titel;
     }
 
+    public User getUser() {
+        return user;
+    }
+    
     /**
+     * This Method adds a comment to the playlist
      * @see Commentable#comment(Comment)
+     * @param comment comment to add
      */
     @Override
     public void comment(Comment comment) {
