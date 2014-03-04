3063b9e6f2dc7bf6a42bddb8fa656d74a023421f
bbraun92 <bastianbraun92@googlemail.com>
2014-03-04 15:35:05 +0100

    Lockable, Medium, News: Kommentare zugefuegt & editiert

--- Dezibel/src/de/dezibel/data/Lockable.java
+++ Dezibel/src/de/dezibel/data/Lockable.java
@@ -1,15 +1,39 @@
 package de.dezibel.data;
 
+/**
+ * Interface which stores functionality to lock and unlock contents which
+ * implement it.
+ *
+ * @author
+ */
 public interface Lockable {
 
-	public abstract void lock();
+    /**
+     * Locks the content, so no user unless the admin can access it.
+     */
+    public abstract void lock();
 
-	public abstract void lock(String text);
+    /**
+     * Locks the content, so no user unless the admin can access it and shows a
+     * locktext to inform about the lock.
+     *
+     * @param text text which informs about the lock
+     */
+    public abstract void lock(String text);
 
-	public abstract void unlock();
+    /**
+     * Unlocks the content, so every user associated with it can access it again
+     * as he used to do before.
+     */
+    public abstract void unlock();
 
-	public abstract boolean isLocked();
+    /**
+     * Checks if the content is locked.
+     *
+     * @return <code>true</code> if its locked, <code>false</code> otherwise
+     */
+    public abstract boolean isLocked();
 
-	public abstract String getLockText();
+    public abstract String getLockText();
 
 }
