0ffe34d0d07b5476b9ff70acbc53817732fb0596
schmokko <ben.knauer@gmail.com>
2014-03-05 11:50:24 +0100

    ProfilPanel angefangen - Profile Tabb: Layout fertig

--- Dezibel/src/de/dezibel/gui/ProfilPanel.java
+++ Dezibel/src/de/dezibel/gui/ProfilPanel.java
@@ -7,6 +7,10 @@ import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
@@ -27,14 +31,33 @@ public class ProfilPanel extends DragablePanel {
     private GridBagConstraints gbc;
     
     private JTextField tfFirstName;
-
+    private JTextField tfLastName;
+    private JTextField tfRole;
+    private JTextField tfPseudonym;
+    private JTextField tfGender;
+    private JTextField tfEmail;
+    private JTextField tfBirthDate;
+    private JTextField tfCity;
+    private JTextField tfCountry;
+    private JTextField tfAboutMe;
+    
+    /**
+     * Constructor of the ProfilPanel class.
+     * @param parent 
+     */
     public ProfilPanel(DezibelPanel parent) {
         super(parent);
         this.createComponents();
-        this.createLayout();
-        //this.add(tabPanel);
+        
+        // Create Layout
+        BorderLayout layout = new BorderLayout();
+        this.setLayout(layout);
+        this.add(tabPanel, BorderLayout.CENTER);
     }
 
+    /**
+     * Method to create tabs.
+     */
     private void createComponents() {
         gbl = new GridBagLayout();
         gbc = new GridBagConstraints();
@@ -57,6 +80,9 @@ public class ProfilPanel extends DragablePanel {
         tabPanel.addTab("Neuigkeiten", null, pnNews);
     }
 
+    /**
+     * This method creates all components in the profile tab.
+     */
     private void createProfileComponents() {
         pnProfile.setLayout(gbl);
         
@@ -71,31 +97,92 @@ public class ProfilPanel extends DragablePanel {
         JLabel lbCountry = new JLabel("Land:");
         JLabel lbAboutMe = new JLabel("Über mich:");
         
-        tfFirstName = new JTextField();
-        tfFirstName.setColumns(20);
+        tfFirstName = new JTextField(25);       
+        tfLastName = new JTextField(25);       
+        tfRole = new JTextField(25);       
+        tfPseudonym = new JTextField(25);       
+        tfGender = new JTextField(25);       
+        tfEmail = new JTextField(25);       
+        tfBirthDate = new JTextField(25);       
+        tfCity = new JTextField(25);       
+        tfCountry = new JTextField(25);       
+        tfAboutMe = new JTextField(25);
+        
+        JButton btnEdit = new JButton("Bearbeiten");
+        btnEdit.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(tfFirstName.isEnabled()){
+                setProfileTextfieldsEditable(false);
+                } else{
+                    setProfileTextfieldsEditable(true);
+                }
+                
+            }
+        });
         
         addComponent(pnProfile, gbl, lbFirstName, 0, 0);
         addComponent(pnProfile, gbl, tfFirstName, 1, 0);
         addComponent(pnProfile, gbl, lbLastName, 0, 1);
+        addComponent(pnProfile, gbl, tfLastName, 1, 1);
         addComponent(pnProfile, gbl, lbRole, 0, 2);
+        addComponent(pnProfile, gbl, tfRole, 1, 2);
         addComponent(pnProfile, gbl, lbPseudonym, 0, 3);
+        addComponent(pnProfile, gbl, tfPseudonym, 1, 3);
         addComponent(pnProfile, gbl, lbGender, 0, 4);
+        addComponent(pnProfile, gbl, tfGender, 1, 4);
         addComponent(pnProfile, gbl, lbEmail, 0, 5);
+        addComponent(pnProfile, gbl, tfEmail, 1, 5);
         addComponent(pnProfile, gbl, lbBirthDate, 0, 6);
+        addComponent(pnProfile, gbl, tfBirthDate, 1, 6);
         addComponent(pnProfile, gbl, lbCity, 0, 7);
+        addComponent(pnProfile, gbl, tfCity, 1, 7);
         addComponent(pnProfile, gbl, lbCountry, 0, 8);
+        addComponent(pnProfile, gbl, tfCountry, 1, 8);
         addComponent(pnProfile, gbl, lbAboutMe, 0, 9);
-
+        addComponent(pnProfile, gbl, tfAboutMe, 1, 9);
+        
+        //TODO Abfrage, ob eigenes Profil
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.insets =  new Insets(10, 0, 0, 0);
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.gridx = 1;
+        gbc.gridy = 10;
+        gbl.setConstraints(btnEdit, gbc);
+        pnProfile.add(btnEdit);
+        
+        setProfileTextfieldsEditable(false);
 
     }
 
-    private void createLayout() {
+    /**
+     * This method enables/disables all textfiels in the profile tab.
+     * 
+     * @param enabled 
+     */
+    private void setProfileTextfieldsEditable(boolean enabled){
+        tfFirstName.setEnabled(enabled);     
+        tfLastName.setEnabled(enabled);       
+        tfRole.setEnabled(enabled);         
+        tfPseudonym.setEnabled(enabled);        
+        tfGender.setEnabled(enabled);         
+        tfEmail.setEnabled(enabled);      
+        tfBirthDate.setEnabled(enabled);        
+        tfCity.setEnabled(enabled);      
+        tfCountry.setEnabled(enabled);       
+        tfAboutMe.setEnabled(enabled);  
+    } 
+            
 
-        BorderLayout layout = new BorderLayout();
-        this.setLayout(layout);
-        this.add(tabPanel, BorderLayout.CENTER);
-    }
-
+    /**
+     * This method adds a components to a given GridBagLayout.
+     * @param cont
+     * @param gbl
+     * @param comp
+     * @param x
+     * @param y 
+     */
     private void addComponent(Container cont,
             GridBagLayout gbl,
             Component comp,
