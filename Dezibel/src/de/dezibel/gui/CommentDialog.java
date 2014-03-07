b7e6d9fc9c0d0eb50f0aa4910a91605ede4c6c91
Tobias Wiedau <t_wied05@wwu.de>
2014-03-07 16:00:09 +0100

    Kommentare für alle!

--- Dezibel/src/de/dezibel/gui/CommentDialog.java
+++ Dezibel/src/de/dezibel/gui/CommentDialog.java
@@ -1,7 +1,10 @@
 package de.dezibel.gui;
 
 import de.dezibel.control.CommentControl;
+import de.dezibel.data.Album;
 import de.dezibel.data.Medium;
+import de.dezibel.data.News;
+import de.dezibel.data.Playlist;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
@@ -18,27 +21,31 @@ import javax.swing.JTextArea;
 class CommentDialog extends JDialog {
 
     private final CommentControl cc;
-    
+
     /**
      * Constructor
+     *
      * @param frame The frame to block
      */
     public CommentDialog(JFrame frame) {
+        super(frame);
+        setModal(true);
         cc = new CommentControl();
     }
-    
+
     /**
      * Handles the commenting for the given medium
+     *
      * @param m The medium to comment
      */
     public void commentMedia(final Medium m) {
         setTitle("Kommentieren");
-        
+
         JLabel lbYourComment = new JLabel("Dein Kommentar");
         final JTextArea taText = new JTextArea();
         JButton btComment = new JButton("Kommentieren");
         JButton btCancel = new JButton("Abbrechen");
-        
+
         btComment.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -46,14 +53,14 @@ class CommentDialog extends JDialog {
                 CommentDialog.this.dispose();
             }
         });
-        
+
         btCancel.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 CommentDialog.this.dispose();
             }
         });
-        
+
         this.setLayout(null);
         lbYourComment.setBounds(5, 5, 100, 32);
         this.add(lbYourComment);
@@ -67,5 +74,131 @@ class CommentDialog extends JDialog {
         this.setResizable(false);
         this.setSize(265, 371);
     }
-    
+
+    /**
+     * Handles the commenting for the given album
+     *
+     * @param a The album to comment
+     */
+    public void commentAlbum(final Album a) {
+        setTitle("Kommentieren");
+
+        JLabel lbYourComment = new JLabel("Dein Kommentar");
+        final JTextArea taText = new JTextArea();
+        JButton btComment = new JButton("Kommentieren");
+        JButton btCancel = new JButton("Abbrechen");
+
+        btComment.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cc.commentAlbum(a, taText.getText());
+                CommentDialog.this.dispose();
+            }
+        });
+
+        btCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CommentDialog.this.dispose();
+            }
+        });
+
+        this.setLayout(null);
+        lbYourComment.setBounds(5, 5, 100, 32);
+        this.add(lbYourComment);
+        taText.setBounds(5, 42, 250, 250);
+        this.add(taText);
+        btComment.setBounds(5, 300, 120, 32);
+        this.add(btComment);
+        btCancel.setBounds(130, 300, 120, 32);
+        this.add(btCancel);
+
+        this.setResizable(false);
+        this.setSize(265, 371);
+    }
+
+    /**
+     * Handles the commenting for the given playlist
+     *
+     * @param p The playlist to comment
+     */
+    public void commentPlaylists(final Playlist p) {
+        setTitle("Kommentieren");
+
+        JLabel lbYourComment = new JLabel("Dein Kommentar");
+        final JTextArea taText = new JTextArea();
+        JButton btComment = new JButton("Kommentieren");
+        JButton btCancel = new JButton("Abbrechen");
+
+        btComment.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cc.commentPlaylist(p, taText.getText());
+                CommentDialog.this.dispose();
+            }
+        });
+
+        btCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CommentDialog.this.dispose();
+            }
+        });
+
+        this.setLayout(null);
+        lbYourComment.setBounds(5, 5, 100, 32);
+        this.add(lbYourComment);
+        taText.setBounds(5, 42, 250, 250);
+        this.add(taText);
+        btComment.setBounds(5, 300, 120, 32);
+        this.add(btComment);
+        btCancel.setBounds(130, 300, 120, 32);
+        this.add(btCancel);
+
+        this.setResizable(false);
+        this.setSize(265, 371);
+    }
+
+    /**
+     * Handles the commenting for the given news
+     *
+     * @param n The news to comment
+     */
+    public void commentNews(final News n) {
+        setTitle("Kommentieren");
+
+        JLabel lbYourComment = new JLabel("Dein Kommentar");
+        final JTextArea taText = new JTextArea();
+        JButton btComment = new JButton("Kommentieren");
+        JButton btCancel = new JButton("Abbrechen");
+
+        btComment.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cc.commentNews(n, taText.getText());
+                CommentDialog.this.dispose();
+            }
+        });
+
+        btCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CommentDialog.this.dispose();
+            }
+        });
+
+        this.setLayout(null);
+        lbYourComment.setBounds(5, 5, 100, 32);
+        this.add(lbYourComment);
+        taText.setBounds(5, 42, 250, 250);
+        this.add(taText);
+        btComment.setBounds(5, 300, 120, 32);
+        this.add(btComment);
+        btCancel.setBounds(130, 300, 120, 32);
+        this.add(btCancel);
+
+        this.setResizable(false);
+        this.setSize(265, 371);
+    }
+
 }
