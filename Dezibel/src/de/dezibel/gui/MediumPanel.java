e683964e6d2f0f1713bdad7bb4e86364115bdea5
Pascal123 <Pascalbrackmann@web.de>
2014-03-10 11:07:22 +0100

    Mediumpanel nun klickbar und läuft ^^

--- Dezibel/src/de/dezibel/gui/MediumPanel.java
+++ Dezibel/src/de/dezibel/gui/MediumPanel.java
@@ -1,10 +1,10 @@
 package de.dezibel.gui;
 
 import java.awt.Dimension;
-import java.util.Date;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.LinkedList;
 import java.util.ListIterator;
-
 import javax.swing.DefaultListModel;
 import javax.swing.GroupLayout;
 import javax.swing.JLabel;
@@ -15,9 +15,7 @@ import javax.swing.ListSelectionModel;
 import javax.swing.SwingConstants;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
-
 import de.dezibel.data.Album;
-import de.dezibel.data.Genre;
 import de.dezibel.data.Label;
 import de.dezibel.data.Medium;
 import de.dezibel.data.User;
@@ -49,18 +47,20 @@ public class MediumPanel extends DragablePanel {
     
     // Labels that will be filled with the medium-properties
     private JLabel 	lbInfoTitle;
-    private JLabel 	lbInfoAlbum;
+    private JLabel	bnInfoAlbum;
     private JLabel 	lbInfoUploadDate;
     private JLabel 	lbInfoAvgRating;
-    private JLabel 	lbInfoArtist;
+    private JLabel bnInfoArtist;
     private JLabel 	lbInfoGenre;
-    private JLabel 	lbInfoLabel;
+    private JLabel	bnInfoLabel;
     private JList<String>		comments;
     private DefaultListModel<String> commentsModel;
     private JScrollPane spComments;
     private JScrollPane spCommentArea;
     private JTextArea 	commentDetail;
     private LinkedList<String> details;
+    private String clickable1 = "<HTML><FONT color=\"#000099\"><U>";
+    private String clickable2 = "</U></FONT></HTML>";
     
 	public MediumPanel(DezibelPanel parent, Medium current) {
 		super(parent);
@@ -79,12 +79,12 @@ public class MediumPanel extends DragablePanel {
 		artist = null;
 		label = null;
 		lbInfoTitle.setText("");
-	    lbInfoAlbum.setText("");
+	    bnInfoAlbum.setText("");
 	    lbInfoUploadDate.setText("");
 	   lbInfoAvgRating.setText("");
-	    lbInfoArtist.setText("");
+	    bnInfoArtist.setText("");
 	    lbInfoGenre.setText("");
-	    lbInfoLabel.setText("");
+	    bnInfoLabel.setText("");
 	    commentsModel.clear();
 	    commentDetail.setText("");
 	    details.clear();
@@ -98,30 +98,31 @@ public class MediumPanel extends DragablePanel {
 		if(currentMedium != null)
 		{
 			lbInfoTitle.setText("");
-			lbInfoAlbum.setText("");
+			bnInfoAlbum.setText("");
 			lbInfoUploadDate.setText("");
 			lbInfoAvgRating.setText("");
-			lbInfoArtist.setText("");
+			bnInfoArtist.setText("");
 			lbInfoGenre.setText("");
-			lbInfoLabel.setText("");
+			bnInfoLabel.setText("");
 			commentsModel.clear();
 			commentDetail.setText("");
 			
 			if(currentMedium.getArtist() != null){
 				artist = currentMedium.getArtist();
-				lbInfoArtist.setText(currentMedium.getArtist().getPseudonym());
+				bnInfoArtist.setText(clickable1 + currentMedium.getArtist().getPseudonym() + clickable2);
 			}
 			else
-				lbInfoArtist.setText("-");
+				bnInfoArtist.setText("-");
 			
 			lbInfoTitle.setText(currentMedium.getTitle());
 			
 			if(currentMedium.getAlbum() != null){
 				album = currentMedium.getAlbum();
-				lbInfoAlbum.setText(currentMedium.getAlbum().getTitle());
+				bnInfoAlbum.setText(clickable1 + currentMedium.getAlbum().getTitle() + clickable2);
+				//bnInfoAlbum.setText(currentMedium.getAlbum().getTitle());
 			}
 			else
-				lbInfoAlbum.setText("-");
+				bnInfoAlbum.setText("-");
 			
 			if(currentMedium.getGenre() != null)
 				lbInfoGenre.setText(currentMedium.getGenre().getName());
@@ -130,10 +131,11 @@ public class MediumPanel extends DragablePanel {
 			
 			if(currentMedium.getLabel() != null){
 				label = currentMedium.getLabel();
-				lbInfoLabel.setText(currentMedium.getLabel().getName());
+				bnInfoLabel.setText(clickable1 + currentMedium.getLabel().getName() + clickable2);
+				//bnInfoLabel.setText(currentMedium.getLabel().getName());
 			}
 			else
-				lbInfoLabel.setText("-");
+				bnInfoLabel.setText("-");
 			
 			if(currentMedium.getUploadDate() != null)
 				lbInfoUploadDate.setText(currentMedium.getUploadDate().toString());
@@ -174,14 +176,52 @@ public class MediumPanel extends DragablePanel {
 	    lbComments = new JLabel("Kommentare:");
 	    
 	    lbInfoTitle = new JLabel("");
-	    lbInfoAlbum = new JLabel("");
+	    bnInfoAlbum = new JLabel("");
 	    lbInfoUploadDate = new JLabel("");
 	    lbInfoAvgRating = new JLabel("");
-	    lbInfoArtist = new JLabel("");
+	    bnInfoArtist = new JLabel("");
 	    lbInfoGenre = new JLabel("");
-	    lbInfoLabel = new JLabel("");
+	    bnInfoLabel = new JLabel("");
 	    
+	    bnInfoArtist.setHorizontalAlignment(SwingConstants.LEFT);
+	    bnInfoArtist.setHorizontalTextPosition( SwingConstants.LEFT );
+	    bnInfoArtist.setOpaque(false);
+	    bnInfoArtist.setBackground(this.getBackground());
+	    bnInfoArtist.addMouseListener(new MouseAdapter(){
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if(e.getClickCount() == 2){
+					onClickArtist();
+				}
+			}
+	    });
 	    
+	    bnInfoLabel.setHorizontalAlignment(SwingConstants.LEFT);
+	    bnInfoLabel.setHorizontalTextPosition( SwingConstants.LEFT );
+	    bnInfoLabel.setOpaque(false);
+	    bnInfoLabel.setBackground(this.getBackground());
+	    bnInfoLabel.addMouseListener(new MouseAdapter(){
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if(e.getClickCount() == 2){
+					onClickLabel();
+				}
+			}
+	    });
+	    
+	    bnInfoAlbum.setHorizontalAlignment(SwingConstants.LEFT);
+	    bnInfoAlbum.setHorizontalTextPosition( SwingConstants.LEFT );
+	    bnInfoAlbum.setOpaque(false);
+	    bnInfoAlbum.setBackground(this.getBackground());
+	    bnInfoAlbum.addMouseListener(new MouseAdapter(){
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if(e.getClickCount() == 2){
+					onClickAlbum();
+				}
+			}
+	    });
+	  
 	    commentsModel = new DefaultListModel<String>();
 	    
 	    comments = new JList<String>(commentsModel);
@@ -228,11 +268,11 @@ public class MediumPanel extends DragablePanel {
 						.addComponent(lbAvgRating)
 						.addComponent(lbComments))
 			    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-			    		.addComponent(lbInfoArtist)
+			    		.addComponent(bnInfoArtist)
 						.addComponent(lbInfoTitle)
-						.addComponent(lbInfoAlbum)
+						.addComponent(bnInfoAlbum)
 						.addComponent(lbInfoGenre)
-						.addComponent(lbInfoLabel)
+						.addComponent(bnInfoLabel)
 						.addComponent(lbInfoUploadDate)
 						.addComponent(lbInfoAvgRating)
 						.addGroup(layout.createSequentialGroup()
@@ -241,28 +281,28 @@ public class MediumPanel extends DragablePanel {
 			);
 
 			layout.setVerticalGroup(layout.createSequentialGroup()
-			    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+			    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
 			    	.addComponent(lbArtist)
-			        .addComponent(lbInfoArtist))
-			    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+			        .addComponent(bnInfoArtist))
+			    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
 			    	.addComponent(lbTitle)
 			        .addComponent(lbInfoTitle))
-			    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+			    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
 			    	.addComponent(lbAlbum)
-			        .addComponent(lbInfoAlbum))
-			    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+			        .addComponent(bnInfoAlbum))
+			    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
 			    	.addComponent(lbGenre)
 			        .addComponent(lbInfoGenre))
-			    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+			    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
 			    	.addComponent(lbLabel)
-			        .addComponent(lbInfoLabel))
-			    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+			        .addComponent(bnInfoLabel))
+			    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
 			    	.addComponent(lbUploadDate)
 			        .addComponent(lbInfoUploadDate))
-			   .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+			   .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
 			    	.addComponent(lbAvgRating)
 			        .addComponent(lbInfoAvgRating))
-			   .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+			   .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
 			    	.addComponent(lbComments)
 			        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
 			        		.addComponent(spComments)
@@ -270,6 +310,10 @@ public class MediumPanel extends DragablePanel {
 			);
 			layout.linkSize(SwingConstants.HORIZONTAL, spComments, spCommentArea);
 			layout.linkSize(SwingConstants.VERTICAL, spComments, spCommentArea);
+			layout.linkSize(SwingConstants.HORIZONTAL,lbInfoTitle, bnInfoAlbum,lbInfoUploadDate,lbInfoAvgRating,
+					bnInfoArtist,lbInfoGenre,bnInfoLabel);
+			layout.linkSize(SwingConstants.VERTICAL,lbInfoTitle, bnInfoAlbum,lbInfoUploadDate,lbInfoAvgRating,
+					bnInfoArtist,lbInfoGenre,bnInfoLabel);
 		this.setLayout(layout);
 	}
 	
@@ -285,7 +329,7 @@ public class MediumPanel extends DragablePanel {
 	 * This function is called, when the user click on the artist of the
 	 * current displayed media.
 	 */
-	private void onClickUser(){
+	private void onClickArtist(){
 		if(artist != null)
 			this.parent.showProfile(artist);
 	}
