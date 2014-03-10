addb234c2f3ec49872ded410017b8c9ea99375c0
schmokko <ben.knauer@gmail.com>
2014-03-10 12:54:42 +0100

    Labelmanager können News erstellen

--- Dezibel/src/de/dezibel/gui/NewsDialog.java
+++ Dezibel/src/de/dezibel/gui/NewsDialog.java
@@ -2,6 +2,9 @@ package de.dezibel.gui;
 
 import de.dezibel.control.NewsControl;
 import de.dezibel.data.Database;
+import de.dezibel.data.Label;
+import de.dezibel.data.User;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
@@ -11,15 +14,18 @@ import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.JComboBox;
 
 /**
  * Dialog to create news
  *
- * @author Tobias
+ * @author Tobias, Benjamin
  */
 class NewsDialog extends JDialog {
 
     private final NewsControl nc;
+    JComboBox<Object> cbAuthor = new JComboBox<>();
+    User loggedInUser = Database.getInstance().getLoggedInUser();
 
     /**
      * Constructor
@@ -37,22 +43,37 @@ class NewsDialog extends JDialog {
      * Initiates the dialog to enter news
      */
     public void init() {
+
+
         setTitle("Neuigkeiten erstellen");
 
         JLabel lbTitle = new JLabel("Titel");
+        JLabel lbAuhor = new JLabel("Author");
+
+        if (loggedInUser.isArtist()) {
+            cbAuthor.addItem(loggedInUser);
+        }
+        if (loggedInUser.isLabelManager()) {
+            for (Label currentLabel : loggedInUser.getManagedLabels()) {
+                cbAuthor.addItem(currentLabel);
+                for (User currentArtist : currentLabel.getArtists()) {
+                    cbAuthor.addItem(currentLabel);
+                }
+            }
+        }
         final JTextField tfTitle = new JTextField();
         final JTextArea taText = new JTextArea();
         JScrollPane sp = new JScrollPane(taText, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
-                                            JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+                JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
         JButton btComment = new JButton("Abschicken");
         JButton btCancel = new JButton("Abbrechen");
 
-        
-        
+
+
         btComment.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                nc.createNews(Database.getInstance().getLoggedInUser(), tfTitle.getText(), taText.getText());
+                nc.createNews(cbAuthor.getSelectedItem(), tfTitle.getText(), taText.getText());
                 NewsDialog.this.dispose();
             }
         });
@@ -65,18 +86,22 @@ class NewsDialog extends JDialog {
         });
 
         this.setLayout(null);
-        lbTitle.setBounds(5, 5, 75, 32);
+        lbTitle.setBounds(10, 5, 75, 32);
         this.add(lbTitle);
-        tfTitle.setBounds(80, 5, 175, 32);
+        tfTitle.setBounds(85, 5, 175, 32);
         this.add(tfTitle);
-        sp.setBounds(5, 42, 250, 250);
+        lbAuhor.setBounds(10, 42, 75, 32);
+        this.add(lbAuhor);
+        cbAuthor.setBounds(85, 42, 175, 32);
+        this.add(cbAuthor);
+        sp.setBounds(10, 79, 250, 250);
         this.add(sp);
-        btComment.setBounds(5, 300, 120, 32);
+        btComment.setBounds(10, 334, 120, 32);
         this.add(btComment);
-        btCancel.setBounds(130, 300, 120, 32);
+        btCancel.setBounds(135, 334, 120, 32);
         this.add(btCancel);
 
         this.setResizable(false);
-        this.setSize(265, 371);
+        this.setSize(265, 391);
     }
 }
