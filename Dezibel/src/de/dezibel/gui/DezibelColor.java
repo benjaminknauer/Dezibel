a89b9256ac030ee3964bd1c74d793066c9966d4c
Henner25 <kein.regen@wolke7.net>
2014-03-11 11:58:54 +0100

    javadoc me all the way

--- Dezibel/src/de/dezibel/gui/DezibelColor.java
+++ Dezibel/src/de/dezibel/gui/DezibelColor.java
@@ -2,7 +2,10 @@ package de.dezibel.gui;
 
 import java.awt.Color;
 
-
+/**
+ * Used colors in dezibel.
+ * @author Paco
+ */
 public class DezibelColor{
 	static Color Background = Color.WHITE;
 	static Color PanelBackground = Color.WHITE;
